e2002d1bb5bda9cea6aa4ce36d1fbf8222a0cb1f
Andy Blight <a.j.blight@leeds.ac.uk>
2019-11-25 15:03:11 +0000

    Fixed read of ID register

--- ad7124spi.py
+++ ad7124spi.py
@@ -16,6 +16,7 @@ class AD7124SPI:
     AD7124_SPI_MODE = 0b00  # Mode 3
     AD7124_REG_CONTROL = 0x01
     AD7124_REG_ID = 0x05
+    
 
     def __init__(self):
         """ Initialises the AD7124 device. """
@@ -45,8 +46,8 @@ class AD7124SPI:
         # Check correct device is present.
         AD7124Id = self.read_id(pi)
         print("init id", AD7124Id)
-        #if AD7124Id != 0x14 or AD7124Id != 0x16:
-        #    raise OSError('ERROR: device on SPI bus is NOT an AD7124!')
+        if AD7124Id != 0x14 and AD7124Id != 0x16:
+            raise OSError('ERROR: device on SPI bus is NOT an AD7124!')
 
     def term(self, pi):
         """ Terminates the AD7124. """
@@ -55,15 +56,30 @@ class AD7124SPI:
 
     def read_id(self, pi):
         """ The value of the ID register is returned. """
-        print("read_id")
-        to_send = bytearray()
-        to_send.append(self.AD7124_REG_ID)
+        result = 0
+        # print("read_id")
+        to_send = []
+        # Build command word
+        command = self._build_command_word(self.AD7124_REG_ID, True)
+        to_send.append(command)
         to_send.append(0)
         (count, data) = pi.spi_xfer(self._spi_handle, to_send)
-        if count < 0:
-            data = []
-        print("read_id", count, data)
-        return data
+        # print("read_id", count, data)
+        # Value is in second byte.
+        if count == 2:
+            result = data[1]
+        return result
+
+    def _build_command_word(self, register, read = False):
+        """ Builds a command value to be sent to the communications register. """
+        command = 0
+        # First bit (bit 7) must be a 0
+        # Second bit (bit 6) is read (1) or write (0).
+        if read:
+            command += (1 << 6)
+        # Remaining 6 bits are register value.
+        command += (register & 0x2f)
+        return command
 
     def read_reg_1(self, pi):
         """ Does a single shot conversion. 
@@ -75,26 +91,28 @@ class AD7124SPI:
         # 100uS is fine.
         time.sleep(0.0001)
         # Command 0x42 plus three bytes for the result.
-        read_command = bytearray([0x42, 0, 0, 0])
+        read_command = [0x42, 0, 0, 0]
         (count, data) = pi.spi_xfer(self._spi_handle, read_command)
         if count < 0:
             data = []
         print("read_reg_1", count, data)
+        
         return data
 
     def _write_reg_control(self, pi, power_mode=0, mode=0, clock_select=0):
         """ Writes to the ADC control register.
+        Default value of the register is 0x00 so parameter defaults of 0 work.
         """
         command = [self.AD7124_REG_CONTROL, 0]
         value = bitstruct.compile('u2u4u2')
         value_bytes = value.pack(power_mode, mode, clock_select)
-        print("_write_reg_control bytes", value_bytes)
+        #print("_write_reg_control bytes", value_bytes)
         value_ints = bitstruct.unpack('u8', value_bytes)
-        print("_write_reg_control ints", value_ints)
+        #print("_write_reg_control ints", value_ints)
         command.append(value_ints[0])
         print("_write_reg_control command", command)
         (count, data) = pi.spi_xfer(self._spi_handle, command)
-        print("_write_reg_control result", count, data)
+        #print("_write_reg_control result", count, data)
 
     def reset(self):
         """ Resets the AD7124 to power up conditions. """
