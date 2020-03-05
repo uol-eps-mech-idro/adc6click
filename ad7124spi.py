d6d0508b3c03faa837e972d90ef91a0521fec50c
Andy Blight <a.j.blight@leeds.ac.uk>
2020-03-05 12:07:39 +0000

    Working SPI driver

--- ad7124spi.py
+++ ad7124spi.py
@@ -2,6 +2,7 @@
 """ Hides the SPI calls from the driver.
 """
 
+import time
 import pigpio
 
 
@@ -10,22 +11,18 @@ class AD7124SPI:
     """
     # Values for SPI communications.  All other values are default.
     # Max SPI baud rate is 5MHz.
-    AD7124_SPI_BAUD_RATE = 5 * 1000 * 1000
-    # AD7124_SPI_BAUD_RATE = 32 * 1000
+    # AD7124_SPI_BAUD_RATE = 5 * 1000 * 1000
+    AD7124_SPI_BAUD_RATE = 32 * 1000
     AD7124_SPI_MODE = 0b00  # Mode 3
 
-    def __init__(self):
-        """ Initialises the AD7124 device. """
-        self._pi = pigpio.pi()
-        self._spi_handle = 0
 
-    def init(self, position):
+    def __init__(self, position):
         """ Initialises the AD7124.
+        position is the Pi2 click shield position number, 1 or 2.
         Throws an exception if it fails.
-        :param position: The position
-        :type position: integer
         """
-        print("\ninit: baud rate:", self.AD7124_SPI_BAUD_RATE)
+        self._pi = pigpio.pi()
+        print("init")
         # The Pi2 click shield only supports main bus, bit 8 = 0.
         spi_flags = 0
         # Set to mode 3
@@ -33,38 +30,30 @@ class AD7124SPI:
         # print("init: flags", spi_flags)
         # Set SPI chip select
         spi_channel = 0
-        if position in (1, 2):
+        if position == 1 or position == 2:
             spi_channel = position - 1
         else:
             raise ValueError('ERROR: position must be 1 or 2')
         # print("init: channel", spi_channel)
         # Open SPI device
-        self._spi_handle = self._pi.spi_open(spi_channel,
-                                             self.AD7124_SPI_BAUD_RATE,
-                                             spi_flags)
-
-    def term(self):
-        """ Terminates the AD7124. """
-        # print("term: pi", pi)
-        self._pi.spi_close(self._spi_handle)
+        self._spi_handle = self._pi.spi_open(spi_channel, self.AD7124_SPI_BAUD_RATE, spi_flags)
 
     def read_register(self, to_send):
-        """ Gets data from the a register.
-        :param to_send: The command with the correct number of padding bytes to
-            receive the data.
-        :type to_send: bytes
-        :returns: The value read from the register as a tuple of
-            (count, list of bytes).
-        :rtype: tuple
+        """ Performs a SPI read using the to_send data.
+        :param to_send: The bytes to send.
+        :returns: the data read by the function.
         """
-        result = self._pi.spi_xfer(self._spi_handle, to_send)
-        # print("_read_register: count", result[0], "data", result[1])
-        return result
+        to_send_string = [hex(i) for i in to_send]
+        print("read_register: to_send:", to_send_string)
+        (count, data) = self._pi.spi_xfer(self._spi_handle, to_send)
+        data_string = [hex(i) for i in data]
+        print("read_register: data:", data_string)
+        return data
 
     def write_register(self, to_send):
-        """ Write the given value to the given register.
-        :param to_send: The command to send.
-        :type to_send: bytes
+        """ The bytes in to_send is wrtten to the SPI bus.
         """
-        # Write the data.
-        self._pi.spi_xfer(self._spi_handle, to_send)
+        to_send_string = [hex(i) for i in to_send]
+        print("write_register:", to_send_string)
+        self._pi.spi_write(self._spi_handle, to_send)
+
--- test_ad7124spi.py
+++ test_ad7124spi.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """ Unit tests for the AD7124 driver. """
-import pigpio
 import unittest
 
 from ad7124spi import AD7124SPI
-from ad7124registers import AD7124RegNames
 
 
 class TestAD7214Spi(unittest.TestCase):
@@ -13,20 +11,18 @@ class TestAD7214Spi(unittest.TestCase):
         """ Verify init works.
         Can throw an exception if the ADC is not connected.
         """
-        self._spi = AD7124SPI()
         position = 1
-        self._spi.init(position)
-
-    def tearDown(self):
-        self._spi.term()
+        self._spi = AD7124SPI(position)
+        to_send = b'\xff\xff\xff\xff\xff\xff\xff\xff'
+        self._spi.write_register(to_send)
 
     def test_read_register(self):
         """ Read the ID register.
-        Should return (1, 0x14).
+        Should return 0x14.
         """
-        to_send = b'\x09\x00\x01'
+        to_send = b'\x45\x00'
         result = self._spi.read_register(to_send)
-        self.assertEqual(1, result[0])
+        self.assertEqual(0xff, result[0])
         self.assertEqual(0x14, result[1])
 
     def test_write_register(self):
@@ -35,16 +31,18 @@ class TestAD7214Spi(unittest.TestCase):
         Then write a new value that changes both bytes.
         Read back to verify change has occurred.
         """
-        to_send = b'\x5a\x00\x00'
+        to_send = b'\x4a\x00\x00'
         result = self._spi.read_register(to_send)
-        self.assertEqual(2, result[0])
-        self.assertEqual(0x0001, result[1])
-        to_send = b'\x1a\x80\x10'
+        self.assertEqual(0xff, result[0])
+        self.assertEqual(0x00, result[1])
+        self.assertEqual(0x01, result[2])
+        to_send = b'\x0a\x80\x10'
         self._spi.write_register(to_send)
-        to_send = b'\x5a\x00\x00'
+        to_send = b'\x4a\x00\x00'
         result = self._spi.read_register(to_send)
-        self.assertEqual(2, result[0])
-        self.assertEqual(0x8010, result[1])
+        self.assertEqual(0xff, result[0])
+        self.assertEqual(0x80, result[1])
+        self.assertEqual(0x10, result[2])
 
 
 if __name__ == '__main__':
