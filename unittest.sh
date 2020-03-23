8c08ef1396f10a763b3329752bdf704007eca10c
Andy Blight <a.j.blight@leeds.ac.uk>
2020-03-23 13:10:09 +0000

    Fixed paths in unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -5,4 +5,5 @@ sudo killall -q pigpiod
 sudo pigpiod
 
 # Run the tests.
-python3 -m unittest -v test_ad7124spi.py test_ad7124driver.py
+python3 -m unittest -v test/test_ad7124spi.py test/test_ad7124driver.py
+
