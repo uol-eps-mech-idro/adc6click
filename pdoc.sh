b5a541636cc4c55138741f2dc2dcfc56ae9dab21
Andy Blight <a.j.blight@leeds.ac.uk>
2020-03-23 14:16:35 +0000

    Added HTML files

--- pdoc.sh
+++ pdoc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Generating documentation.  Please ignore any output until this script exits."
+# Prevent any old files from being included.
+rm -rf html
 # pdoc fails when trying to parse the setup.py file so rename until the doc are produced. 
 mv setup.py setup.not_py
 pdoc --html .
 mv setup.not_py setup.py
 
-echo "HTML produced in html dir."
+echo "All done. HTML produced in html dir."
 
